03c4eeeac0d031c6d9b7bc1c76255b03b7126d55
Tate Wilson <tate_wilson135@mymail.eku.edu>
2024-08-27 16:12:55 -0400

    Initial Commit

---
+++ MemoApp_TateWilson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoApp_TateWilson
+//
+//  Created by Wilson, Tate M. on 8/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
